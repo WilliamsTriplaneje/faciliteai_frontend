343ddecf3fb3f0cef74960acac110e7a3c34bd91
Brayan Quirino <brayan.molk5@gmail.com>
2020-09-04 11:37:04 -0300

    configuração no package.json para iniciar server local

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated, isAdmin } from "./auth";
 
 import "./assets/plugins/nucleo/css/nucleo.css";
+// import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./assets/scss/argon-dashboard-react.scss";
 
 import ProviderLayout from "./layouts/Admin.js";
