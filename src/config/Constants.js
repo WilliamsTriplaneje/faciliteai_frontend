6bd764df594b716387bd2b9b245c2b4f03ca3fa6
Guilherme Cunha <guiscunha@gmail.com>
2020-09-16 12:50:48 -0300

    Ajustando logs Dotenv

--- src/config/Constants.js
+++ src/config/Constants.js
@@ -8,7 +8,6 @@ export const LOCAL_USER = 'User'
 
 export const AUTHENTICATED_ROUTE_PREFIX = 'app'
 
-console.log(`Reconhecendo a chave do stripe: ${process.env.REACT_APP_STRIPE_PUBLIC}`)
 export const STRIPE_PUBLIC = process.env.REACT_APP_STRIPE_PUBLIC
 
 export const API_URL =  process.env.REACT_APP_API_URL
--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,10 @@ import axios from "axios";
 import { getToken } from "../auth";
 import { API_URL } from '../config/Constants'
 
+console.log(`Configurando API com a url: ${API_URL}`)
+
 const api = axios.create({
   baseURL: API_URL,
-  // baseURL: "http://localhost:3333",
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
     "Access-Control-Allow-Origin": "*",
